f748f6ab380836c34edec660d2554fabfe346e3e
aridai <aridai.net@outlook.jp>
2021-02-06 09:13:11 +0900

    動画リスト要素の型を直和型に変更

--- lib/ui/search/list/list_element.dart
+++ lib/ui/search/list/list_element.dart
@@ -1,14 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:youtube_search_app/model/video.dart';
 
+part 'list_element.freezed.dart';
+
 //  検索ページのリスト要素
-abstract class ListElement {}
+@freezed
+abstract class ListElement with _$ListElement {
+  //  動画要素
+  const factory ListElement.video(Video video) = VideoElement;
 
-//  動画要素
-class VideoElement extends ListElement {
-  VideoElement(this.model);
-
-  final Video model;
+  //  ProgressIndicator要素
+  const factory ListElement.indicator() = ProgressIndicatorElement;
 }
-
-//  末尾のProgressIndicator要素
-class ProgressIndicatorElement extends ListElement {}
