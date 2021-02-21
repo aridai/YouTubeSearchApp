2821aa38b3b152f7e5e970f97f652c786b04f903
aridai <aridai.net@outlook.jp>
2021-02-21 19:13:10 +0900

    Hiveを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:youtube_search_app/dependency.dart';
 import 'package:youtube_search_app/my_app.dart';
 
@@ -7,6 +9,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+  await Hive.initFlutter();
   await Dependency.setup();
+
   runApp(MyApp());
 }
